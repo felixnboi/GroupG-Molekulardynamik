9852bd7205becbfa916191dc0c4ebcc938a6f7af
Felix Zhengbuo Guan <ge47pov@mytum.de>
2024-04-29 22:58:02 +0200

    we worked on task 3

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -1,12 +1,19 @@
 
 #include "FileReader.h"
 #include "outputWriter/XYZWriter.h"
+#include "outputWriter/VTKWriter.h"
 #include "utils/ArrayUtils.h"
 
 #include <iostream>
 #include <list>
+#include <math.h>
 
 /**** forward declaration of the calculation functions ****/
+
+/**
+ * calculate the euclidean norm of the distance between two particles
+ */
+double euclidean_norm_x(Particle particle1, Particle particle2);
 
 /**
  * calculate the force for all particles
@@ -73,25 +80,68 @@ int main(int argc, char *argsv[]) {
 }
 
 void calculateF() {
-  std::list<Particle>::iterator iterator;
-  iterator = particles.begin();
+  std::list<Particle>::iterator iterator_i;
+  iterator_i = particles.begin();
+  std::list<Particle>::iterator iterator_j;
 
-  for (auto &p1 : particles) {
-    for (auto &p2 : particles) {
-      // @TODO: insert calculation of forces here!
+  for(auto &p : particles){
+    auto old_F = p.getOldF();
+    auto cur_F = p.getF();
+    for(int i = 0; i<3 ;i++){
+      old_F.at(i) = cur_F.at(i);
+      cur_F.at(i) = 0;
+    }
+  }
+  for (int i = 0; i<particles.size()-1; i++) {
+    auto cur_particle_i = *iterator_i;
+    auto m_i = cur_particle_i.getM();
+    auto cur_x_i = cur_particle_i.getX();
+    auto cur_F_i = cur_particle_i.getF();
+    for (iterator_j = iterator_i++; iterator_j!=particles.end(); iterator_j++) {
+      auto cur_particle_j = *iterator_j;
+      auto m_j = cur_particle_j.getM();
+      auto norm = euclidean_norm_x(cur_particle_i, cur_particle_j);
+      auto cur_x_j = cur_particle_j.getX(); 
+      auto cur_F_j = cur_particle_j.getF();
+      for(int k = 0; k<3; k++){
+        double tmp = m_i * m_j / pow(norm, 3) * (cur_x_j.at(k) - cur_x_i.at(k));
+        cur_F_i.at(k) += tmp;
+        cur_F_j.at(k) -= tmp;
+      }
     }
   }
 }
 
+double euclidean_norm_x(Particle particle1, Particle particle2){
+  double sum = 0;
+  for (int i = 0; i<3; i++){
+    sum += pow(particle1.getX().at(i) - particle2.getX().at(i), 2);
+  }
+  return pow(sum, 0.5);
+}
+
 void calculateX() {
   for (auto &p : particles) {
-    // @TODO: insert calculation of position updates here!
+    auto m = p.getM();
+    auto cur_x = p.getX();
+    auto cur_v = p.getV();
+    auto cur_F = p.getF();
+    for(int i = 0; i<3; i++){
+      cur_x.at(i) = cur_x.at(i) + delta_t * cur_v.at(i) + delta_t * delta_t * cur_F.at(i) / (2*m); 
+    }
   }
 }
 
 void calculateV() {
   for (auto &p : particles) {
-    // @TODO: insert calculation of veclocity updates here!
+    auto m = p.getM();
+    auto cur_x = p.getX();
+    auto cur_v = p.getV();
+    auto cur_F = p.getF();
+    auto old_F = p.getOldF();
+    for(int i = 0; i<3; i++){
+      cur_v.at(i) = cur_v.at(i) + delta_t * (old_F.at(i) + cur_F.at(i)) / (2*m);
+    }
   }
 }
 
