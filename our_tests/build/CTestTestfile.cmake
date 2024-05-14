3017e229f995ca60df3525674c5db66bf1ad97f8
Felix Guan <ge47pov@mytum.de>
2024-05-14 13:00:09 +0200

    refactored tests

--- our_tests/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/masha_lelyukh/BachelorPraktikum/GroupG-Molekulardynamik/our_tests
-# Build directory: /home/masha_lelyukh/BachelorPraktikum/GroupG-Molekulardynamik/our_tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/masha_lelyukh/BachelorPraktikum/GroupG-Molekulardynamik/our_tests/build/ParticleContainerTest[1]_include.cmake")
