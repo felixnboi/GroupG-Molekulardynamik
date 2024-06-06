f0978169cfa52ede16a07f4168f36e125905c364
Maria <maria.gitanes@gmail.com>
2024-06-06 11:30:30 +0200

    comments

--- src/ParticleContainers/ParticleContainerOld.h
+++ src/ParticleContainers/ParticleContainerOld.h
@@ -3,7 +3,6 @@
  
 
 /**
- * @class ParticleContainerOld
  * @brief A container for managing particles.
  * 
  * This class represents a container for managing particles. It provides methods to add particles
@@ -12,22 +11,42 @@
 class ParticleContainerOld : public ParticleContainer{
 
 public:
-/**
- * @brief Constructor for ParticleContainerOld
-*/
+    /**
+     * @brief Constructor for ParticleContainerOld.
+    */
     ParticleContainerOld();
-    
+    /**
+    * @brief Destructor for ParticleContainerOld.
+    */
     ~ParticleContainerOld();
-
+    /**
+     * @brief Reserves space for a given number of particles.
+     * @param size The number of particles to reserve space for.
+     */
     void reserve(size_t size) override;
-
+    /**
+     * @brief Adds a particle to the container.
+     * @param particle The particle to add.
+     */
     void addParticle(const std::shared_ptr<Particle> particle) override;
-    
+    /**
+     * @brief Returns an iterator to the beginning of the particles.
+     * @return An iterator to the beginning of the particles.
+     */
     ParticleIterator begin() override;
-
+    /**
+     * @brief Returns an iterator to the end of the particles.
+     * @return An iterator to the end of the particles.
+     */
     ParticleIterator end() override;
-
+    /**
+     * @brief Gets the particles in the container.
+     * @return A reference to the vector of particles.
+     */
     const std::vector<std::shared_ptr<Particle>>& getParticles() override;
-
+    /**
+     * @brief Gets all possible pairs of particles.
+     * @return A vector of pairs of particles.
+     */
     std::vector<std::array<std::shared_ptr<Particle>,2>> getParticlePairs() override;
 };
